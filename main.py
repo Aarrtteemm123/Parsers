f7a887fc04cda1587152e0ddca454663a52de693
Artem <artem@gmail.com>
2019-12-16 13:21:31 +0200

    add timer

--- main.py
+++ main.py
@@ -4,11 +4,25 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib, re, time
 
+def get_html_all_vac(dictionary):
+    header = """ <html> <body> <h3 style="font-weight: bolder;color:black;">
+                Hello!<br>See vacancies on Dou.ua</h3>"""
+    html=header
+    for language in list(dictionary.keys()):
+        title = """ <br><h3 style="font-weight: bolder;color:black;">
+                Language  """ + language + """<br>
+                </h3><br><hr><br>"""
+        body = ""
+        for vac in dictionary[language]:
+            body += """<p style="font-weight: normal;color:black;"> """ + vac + """ </p>"""
+        html+=title+body
+    end = "</body> </html>"
+    return html + end
 
-def get_html(key, lst):
+def get_html_new_vac(key, lst):
     title = """ <html> <body> <h3 style="font-weight: bolder;color:black;">
         Hello,good news!<br>See new vacancies from """ + key + """ on Dou.ua<br>
-        </h2><br><hr><br>"""
+        </h3><br><hr><br>"""
     body = ""
     for str in lst:
         body += """<p style="font-weight: normal;color:black;"> """ + str + """ </p>"""
@@ -36,47 +50,56 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')  # для открытия headless-браузера
 browser = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=options)
 
-base_urls = {'Java': 'https://jobs.dou.ua/vacancies/?city=%D0%92%D0%B8%D0%BD%D0%BD%D0%B8%D1%86%D0%B0&category=Java'}
+base_urls = {'Java': 'https://jobs.dou.ua/vacancies/?city=%D0%92%D0%B8%D0%BD%D0%BD%D0%B8%D1%86%D0%B0&category=Java',
+             'Python':'https://jobs.dou.ua/vacancies/?city=%D0%97%D0%B0%D0%BF%D0%BE%D1%80%D0%BE%D0%B6%D1%8C%D0%B5&category=Python'}
 base_urls_keys = list(base_urls.keys())
 vac_list = []
+vac_by_lan={}
 
 from_email = read_file('from_email.txt')
 to_email = read_file('to_email.txt')
 password = read_file('password.txt')
 title = 'Dou-jobs(Python,Java)'
+start_time = time.time()
 
-for key in base_urls_keys:
-    browser.get(base_urls[key])
-    soup = BeautifulSoup(browser.page_source, 'lxml')
-    # шукаєм потрібні блоки
-    title_block = soup.find_all('div', attrs={'class': 'b-inner-page-header'})
-    title = title_block[0].find('h1').text
-    first_word = re.split(' ', title)[0]
-    if first_word != 'Нет':
-        number_vacancies = int(first_word)
-        print(number_vacancies)
-        while True:
-            try:
-                browser.find_element_by_link_text('Больше вакансий').click()
-                time.sleep(1)
-            except:
-                break
+while True:
+    for key in base_urls_keys:
+        browser.get(base_urls[key])
         soup = BeautifulSoup(browser.page_source, 'lxml')
-        divs = soup.find_all('div', attrs={'class': 'vacancy'})  # шукаєм потрібні блоки
-        print(len(divs))
-        new_vac = []
-        for div in divs:
-            try:
-                title = div.find('a', attrs={'class': 'vt'}).text  # назва вакансій
-                city = div.find('span', attrs={'class': 'cities'}).text  # місто
-                link = div.find('a', attrs={'class': 'vt'})['href']  # силка
-                string = title + " (" + city + ")" + " " + link
-                if not vac_list.__contains__(string):
-                    new_vac.append(string)
-                    vac_list.append(string)
-            except:
-                pass
-        if len(new_vac) != 0:
-            send_email(from_email, to_email, password, get_html(key, new_vac), title)
-
+        # шукаєм потрібні блоки
+        title_block = soup.find_all('div', attrs={'class': 'b-inner-page-header'})
+        title = title_block[0].find('h1').text
+        first_word = re.split(' ', title)[0]
+        if first_word != 'Нет':
+            number_vacancies = int(first_word)
+            print(number_vacancies)
+            while True:
+                try:
+                    browser.find_element_by_link_text('Больше вакансий').click()
+                    time.sleep(1)
+                except:
+                    break
+            soup = BeautifulSoup(browser.page_source, 'lxml')
+            divs = soup.find_all('div', attrs={'class': 'vacancy'})  # шукаєм потрібні блоки
+            print(len(divs))
+            new_vac = []
+            for div in divs:
+                try:
+                    title = div.find('a', attrs={'class': 'vt'}).text  # назва вакансій
+                    city = div.find('span', attrs={'class': 'cities'}).text  # місто
+                    link = div.find('a', attrs={'class': 'vt'})['href']  # силка
+                    string = title + " (" + city + ")" + " " + link
+                    if not vac_list.__contains__(string):
+                        new_vac.append(string)
+                        vac_list.append(string)
+                except:
+                    pass
+            #if len(new_vac) != 0:
+             #   send_email(from_email, to_email, password, get_html_new_vac(key, new_vac), title)
+            finish_time = time.time()
+            vac_by_lan[key]=vac_list
+            if finish_time-start_time>10:
+                start_time = finish_time
+                send_email(from_email, to_email, password, get_html_all_vac(vac_by_lan), title)
+    time.sleep(2)
 browser.quit()
