2553d71735b877e84b91d537e6cc14562be50a9c
Artem <artem@gmail.com>
2019-12-19 17:59:46 +0200

    added login,search user function.Added open subscribers window function

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
+import pyautogui
 
-options = webdriver.ChromeOptions()  # option Chrome
-options.add_argument('headless')  # to open the browser in Headless mode
-browser = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=options)
+def read_file(filename):  # read data from file
+    with open(filename, 'r') as file:
+        return file.read()
+
+browser = webdriver.Chrome(executable_path='chromedriver.exe')
 
 base_url = "https://www.instagram.com/accounts/login/?hl=uk&source=auth_switcher"
-login = ""
-password = ""
-username = ""
+login = read_file('login.txt')
+password = read_file('password.txt')
+profile_url = read_file('profile_url.txt')
+
+browser.get(base_url)
+time.sleep(2)
+username_form = browser.find_element_by_class_name('gr27e ').find_element_by_name('username')
+password_form = browser.find_element_by_class_name('gr27e ').find_element_by_name('password')
+
+username_form.send_keys(login)
+password_form.send_keys(password)
+
+pyautogui.keyDown('enter')
+pyautogui.keyUp('enter')
+time.sleep(2)
+
+browser.get(profile_url)
+browser.find_elements_by_class_name('Y8-fY ')[1].click()
+
